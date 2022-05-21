55fff7a86e0a3aa5b139961fc176347be5605577
Maruthamalai <58974695+Maruthamalai@users.noreply.github.com>
2022-05-21 21:55:27 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,13 @@
 #include <vector>
 
+typedef struct
+{
+    /* data */
+    float average;
+    float max;
+    float min;
+}Stats;
+
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& );
 }
