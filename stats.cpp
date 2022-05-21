1bd0ef0c59c94d98e8eb37b3dea9dab71f074f35
Maruthamalai <maruthamalai67@gmail.com>
2022-05-21 21:50:37 +0530

    Implement function to calculate statistics

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float> &elements)
+{
+    Stats computedStats = {0.0, 0.0, 0.0};
+    float total = 0.0;
+    for (auto& it:elements)
+    {
+        total+= it;
+
+        if(it>computedStats.max)
+        {
+            computedStats.max = it;
+        }
+
+        if(it<computedStats.min)
+        {
+            computedStats.min = it;
+        }
+    }
+computedStats.average = total / elements.size();
+
+return computedStats;
+
 }
