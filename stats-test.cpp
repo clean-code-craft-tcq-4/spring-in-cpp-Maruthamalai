72a91958c615e1999a74f35fd918c1303837235b
Maruthamalai <58974695+Maruthamalai@users.noreply.github.com>
2022-05-22 21:48:22 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,4 +1,4 @@
-#if 0
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
@@ -8,7 +8,8 @@
 
 using namespace std;
 TEST_CASE("reports average, minimum and maximum") {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    Statistics::StatisticsCalc* computeObj = new Statistics::StatisticsCalc();
+    auto computedStats = computeObj->ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
@@ -16,13 +17,14 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    auto computedStats = Statistics::ComputeStatistics({});
+    Statistics::StatisticsCalc* computeObj = new Statistics::StatisticsCalc();
+    auto computedStats = computeObj->ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
-    REQUIRE(isnan(computedStats.average));
-    REQUIRE(isnan(computedStats.min));
-    REQUIRE(isnan(computedStats.max));
+    REQUIRE(isnan(computedStats.average) == 1);
+    REQUIRE(isnan(computedStats.min) ==1);
+    REQUIRE(isnan(computedStats.max) == 1);
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
